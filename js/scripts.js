aab54509b504fb80da13ea7cfe817ea8f4beefe8
Ykro314 <lolkokpol@gmal.com>
2016-06-09 16:02:52 +0300

    working newsfeed without ui animations

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,37 @@
+var month = {
+  "0": "Jan",
+  "1": "Feb",
+  "2": "Mar",
+  "3": "Apr",
+  "4": "May",
+  "5": "Jun",
+  "6": "Jul",
+  "7": "Aug",
+  "8": "Sep",
+  "9": "Oct", 
+  "10": "Nov",
+  "11": "Dec",
+}
+
 var newsArray = data.news;
 var helpArray = [];
 
-newsArray.sort( function( a, b) {
-  a = new Date( Date.parse( a.date ) ).getMonth();
-  b = new Date( Date.parse( b.date ) ).getMonth();
-  return a - b;
-})
+//newsArray.sort( function( a, b) {
+//  a = new Date( Date.parse( a.date ) ).getMonth();
+//  b = new Date( Date.parse( b.date ) ).getMonth();
+//  return a - b;
+//});
 
-var groupedArray = createArrays( newsArray );
+function sortElementsInArray( array ) {
+  array.sort( function( a, b) {
+    a = new Date( Date.parse( a.date ) ).getMonth();
+    b = new Date( Date.parse( b.date ) ).getMonth();
+    return a - b;
+  });
+  return array;
+}
+
+//var groupedArray = createArrays( newsArray );
 
 
 function createArrays( list ) {
@@ -34,6 +58,7 @@ function getRandomRecordFromArray( array ) {
   if( array.length > 0 ) {
     var index = randomInteger(0, array.length - 1);
     console.log( array[index].header, index );
+    helpArray.push( array[ index ] );
     array.splice( index, 1 );
     getRandomRecordFromArray( array );
   }
@@ -43,11 +68,74 @@ function getRandomRecordFromArray( array ) {
   }
 }
 
-groupedArray.forEach( function( el, i, arr) {
-  getRandomRecordFromArray( el );
+//groupedArray.forEach( function( el, i, arr) {
+//  getRandomRecordFromArray( el );
+//});
+
+//console.log( helpArray );
+
+function showNews( array ) {
+  var header = document.querySelector( ".blog__news-title" );
+  var date = document.querySelector( ".blog__news-date" );
+  for( var i = 0; i < array.length; i++ ) {
+    
+    (function(){
+    var j = i;
+    setTimeout( function timer() {
+      
+      function formatDate( date ) {
+        date = new Date( Date.parse( array[j].date ) );
+        function getMonthName( monthNumber, enumMonthObj ) {
+          for( key in enumMonthObj ) {
+            if( key == monthNumber ) {
+              return enumMonthObj[key];
+            }
+          }
+        }
+        var properDate = getMonthName( date.getMonth(), month ) + " " +  date.getDate() + ", " + date.getFullYear();
+        return properDate;
+      }
+      function formatText( text, maxSymbols ) {
+        if( text.length > maxSymbols ) {
+          var formatedText = text.slice( 0, maxSymbols ) + "...";
+          return formatedText;
+        }
+        else {
+          return text;
+        }
+      }
+      
+      header.textContent = formatText( array[j].header, 45 );
+      date.textContent = formatDate( array[j].date );
+      if( j == array.length - 1 ) {
+        createAndDispatchCustomEvent( "newsend" );
+      }
+    }, 15000 * j );
+    })();
+    
+  }
+} 
+
+function createAndDispatchCustomEvent( eventType ) {
+  var evt = new CustomEvent( eventType );
+  window.dispatchEvent( evt );
+}
+//showNews( helpArray );
+
+function init( array ) {
+  var sortedArray = sortElementsInArray( array );
+  var groupedArray = createArrays( sortedArray );
+  groupedArray.forEach( function( el, i, arr) {
+    getRandomRecordFromArray( el );
+  });
+  showNews( helpArray );
+}
+
+window.addEventListener( "newsend", function( event ) {
+  init( newsArray );
 })
 
-console.log( groupedArray );
+init( newsArray );
 
 //var testRandomArray = groupedArray[3];
 //newsArray.forEach( function( el, i, arr ) {
